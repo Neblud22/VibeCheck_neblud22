800bcaf36a4e04e3b59c93013cc65696e24ee567
neblud22 <neblud22@htl-kaindorf.at>
2026-02-09 09:08:14 +0100

    [backend] add pojo classes

---
+++ Backend/src/main/java/nebel/backend/pojo/Rating.java
@@ -0,0 +1,12 @@
+package nebel.backend.pojo;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Rating {
+
+}
