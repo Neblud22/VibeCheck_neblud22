26baff0da046f59d24d6b31ea15c20f5634c5ea4
neblud22 <neblud22@htl-kaindorf.at>
2026-02-11 11:26:19 +0100

    [backend] add ArtistService into ArtistController

--- Backend/src/main/java/nebel/backend/controller/ArtistController.java
+++ Backend/src/main/java/nebel/backend/controller/ArtistController.java
@@ -1,9 +1,13 @@
 package nebel.backend.controller;
 
+import nebel.backend.service.ArtistService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/artists")
 public class ArtistController {
+
+    private final ArtistService artistService;
+
 }
