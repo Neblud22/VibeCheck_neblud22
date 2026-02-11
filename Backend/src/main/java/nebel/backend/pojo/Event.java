27037606b0d802713742a6b69c481a4a1fd8c645
neblud22 <neblud22@htl-kaindorf.at>
2026-02-11 10:59:30 +0100

    [backend] event pojo add

--- Backend/src/main/java/nebel/backend/pojo/Event.java
+++ Backend/src/main/java/nebel/backend/pojo/Event.java
@@ -1,12 +1,24 @@
 package nebel.backend.pojo;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
 
 @Entity
 @Data
 @NoArgsConstructor
 public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long eventId;
 
+    private String title;
+    private String location;
+    private LocalDate eventDate;
+    private String imageUrl;
 }
