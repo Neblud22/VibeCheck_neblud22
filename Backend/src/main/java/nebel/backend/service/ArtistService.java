ce3325fcdaf810970afc78152c66d03c13eb10a7
neblud22 <neblud22@htl-kaindorf.at>
2026-02-11 11:20:26 +0100

    [backend] add @RequiredArgsConstructor to ArtistService

--- Backend/src/main/java/nebel/backend/service/ArtistService.java
+++ Backend/src/main/java/nebel/backend/service/ArtistService.java
@@ -1,7 +1,9 @@
 package nebel.backend.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ArtistService {
 }
