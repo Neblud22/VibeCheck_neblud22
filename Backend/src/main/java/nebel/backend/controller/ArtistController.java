7a21b294224ebe08506309244fe2e2ac0a3f8a59
neblud22 <neblud22@htl-kaindorf.at>
2026-02-11 11:27:48 +0100

    [backend] add GET /api/artists

--- Backend/src/main/java/nebel/backend/controller/ArtistController.java
+++ Backend/src/main/java/nebel/backend/controller/ArtistController.java
@@ -1,13 +1,23 @@
 package nebel.backend.controller;
 
+import nebel.backend.pojo.Artist;
 import nebel.backend.service.ArtistService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/artists")
 public class ArtistController {
 
     private final ArtistService artistService;
+
+    @GetMapping
+    public List<Artist> getAllArtists() {
+        return artistService.getAllArtists();
+    }
+
 
 }
