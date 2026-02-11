5145d3e50fb40dbff05649e77ed54c05803fb2fe
neblud22 <neblud22@htl-kaindorf.at>
2026-02-11 10:59:38 +0100

    [backend] rating pojo add

--- Backend/src/main/java/nebel/backend/pojo/Rating.java
+++ Backend/src/main/java/nebel/backend/pojo/Rating.java
@@ -1,12 +1,24 @@
 package nebel.backend.pojo;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Data
 @NoArgsConstructor
 public class Rating {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long ratingId;
+
+    private Integer stars;
+    private String comment;
+    private LocalDateTime createdAt;
 }
