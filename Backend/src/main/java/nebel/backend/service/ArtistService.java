a4a5eae689d764423ff9678faf19f775dd918e2f
neblud22 <neblud22@htl-kaindorf.at>
2026-02-11 11:21:23 +0100

    [backend] add ArtistRepo as a variable

--- Backend/src/main/java/nebel/backend/service/ArtistService.java
+++ Backend/src/main/java/nebel/backend/service/ArtistService.java
@@ -1,9 +1,11 @@
 package nebel.backend.service;
 
 import lombok.RequiredArgsConstructor;
+import nebel.backend.repo.ArtistRepo;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ArtistService {
+    private final ArtistRepo artistRepo;
 }
