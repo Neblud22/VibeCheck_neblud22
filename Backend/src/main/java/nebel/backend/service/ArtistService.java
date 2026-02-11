dd4fdd84dc3b4541c55caaac68e158fd861222e5
neblud22 <neblud22@htl-kaindorf.at>
2026-02-11 11:22:04 +0100

    [backend] add getAllArtists method to ArtistService

--- Backend/src/main/java/nebel/backend/service/ArtistService.java
+++ Backend/src/main/java/nebel/backend/service/ArtistService.java
@@ -1,11 +1,18 @@
 package nebel.backend.service;
 
 import lombok.RequiredArgsConstructor;
+import nebel.backend.pojo.Artist;
 import nebel.backend.repo.ArtistRepo;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ArtistService {
     private final ArtistRepo artistRepo;
+
+    public List<Artist> getAllArtists() {
+        return artistRepo.findAll();
+    }
 }
