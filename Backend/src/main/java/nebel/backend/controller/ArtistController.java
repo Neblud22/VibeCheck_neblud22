7106a39bcfe6db5ced551fde6e188e7da744449e
neblud22 <neblud22@htl-kaindorf.at>
2026-02-11 11:28:35 +0100

    [backend] add GET /api/artists/{artistId} and @RequiredArgsConstructor

--- Backend/src/main/java/nebel/backend/controller/ArtistController.java
+++ Backend/src/main/java/nebel/backend/controller/ArtistController.java
@@ -1,8 +1,10 @@
 package nebel.backend.controller;
 
+import lombok.RequiredArgsConstructor;
 import nebel.backend.pojo.Artist;
 import nebel.backend.service.ArtistService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/artists")
+@RequiredArgsConstructor
 public class ArtistController {
 
     private final ArtistService artistService;
@@ -19,5 +22,8 @@ public class ArtistController {
         return artistService.getAllArtists();
     }
 
-
+    @GetMapping("/{artistId}")
+    public Artist getArtistById(@PathVariable Long artistId) {
+        return artistService.getArtistById(artistId);
+    }
 }
