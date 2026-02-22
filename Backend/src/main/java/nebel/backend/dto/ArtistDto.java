f2d123690f03baae27d864b192c67324ed3c1670
neblud22 <neblud22@htl-kaindorf.at>
2026-02-22 19:19:55 +0100

    [backend] add @AllArgsConstructor to ArtistDto

--- Backend/src/main/java/nebel/backend/dto/ArtistDto.java
+++ Backend/src/main/java/nebel/backend/dto/ArtistDto.java
@@ -1,8 +1,10 @@
 package nebel.backend.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class ArtistDto {
     private String firstName;
     private String lastName;
