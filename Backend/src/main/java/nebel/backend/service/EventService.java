043ad95d1095cacc1c1122c873f4a8a42c3daab1
neblud22 <neblud22@htl-kaindorf.at>
2026-02-23 08:38:57 +0100

    [backend] add Annotations in EventService

--- Backend/src/main/java/nebel/backend/service/EventService.java
+++ Backend/src/main/java/nebel/backend/service/EventService.java
@@ -1,4 +1,9 @@
 package nebel.backend.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class EventService {
 }
