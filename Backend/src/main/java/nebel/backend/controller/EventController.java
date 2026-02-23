3e48de1bdf0e25e24d29a8b482e75af6b5b29b32
neblud22 <neblud22@htl-kaindorf.at>
2026-02-23 08:47:23 +0100

    [backend] add @RequestMapping mit api in EventController

--- Backend/src/main/java/nebel/backend/controller/EventController.java
+++ Backend/src/main/java/nebel/backend/controller/EventController.java
@@ -1,8 +1,10 @@
 package nebel.backend.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/events")
 public class EventController {
 
 }
