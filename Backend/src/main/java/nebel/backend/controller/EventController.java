500b2fcf94abdc7c07298cd3f2f2df1ef92f999d
neblud22 <neblud22@htl-kaindorf.at>
2026-02-23 08:47:53 +0100

    [backend] add @RequiredArgsConstructor in EventController

--- Backend/src/main/java/nebel/backend/controller/EventController.java
+++ Backend/src/main/java/nebel/backend/controller/EventController.java
@@ -1,10 +1,12 @@
 package nebel.backend.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/events")
+@RequiredArgsConstructor
 public class EventController {
 
 }
