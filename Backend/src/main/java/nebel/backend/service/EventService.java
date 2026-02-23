65e6bbe5db85e5bccd193962c0b4df9f88546101
neblud22 <neblud22@htl-kaindorf.at>
2026-02-23 08:44:06 +0100

    [backend] add Repo in EventService

--- Backend/src/main/java/nebel/backend/service/EventService.java
+++ Backend/src/main/java/nebel/backend/service/EventService.java
@@ -1,9 +1,14 @@
 package nebel.backend.service;
 
 import lombok.RequiredArgsConstructor;
+import nebel.backend.repo.EventRepo;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class EventService {
+
+    private final EventRepo eventRepository;
+
+
 }
