cd45b3b3adc493810e9f0618bf7946afd07c3c77
neblud22 <neblud22@htl-kaindorf.at>
2026-02-23 08:26:20 +0100

    [backend] add event variables in EventDto

--- Backend/src/main/java/nebel/backend/dto/EventDto.java
+++ Backend/src/main/java/nebel/backend/dto/EventDto.java
@@ -1,4 +1,12 @@
 package nebel.backend.dto;
 
+import java.time.LocalDate;
+
 public class EventDto {
+    private String title;
+    private String location;
+    private LocalDate eventDate;
+    private String imageUrl;
+    private String description;
+
 }
