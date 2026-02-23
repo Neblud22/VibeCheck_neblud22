75f1208f184f01fc3df857f7084287c169e987ca
neblud22 <neblud22@htl-kaindorf.at>
2026-02-23 08:16:26 +0100

    [backend] add JpaRepo extension

--- Backend/src/main/java/nebel/backend/repo/EventRepo.java
+++ Backend/src/main/java/nebel/backend/repo/EventRepo.java
@@ -1,4 +1,7 @@
 package nebel.backend.repo;
 
-public interface EventRepo {
+import nebel.backend.pojo.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventRepo extends JpaRepository<Event, Long> {
 }
