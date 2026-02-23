d4e6702996d487d16b6f671c7af5f3d5519cc560
neblud22 <neblud22@htl-kaindorf.at>
2026-02-23 08:18:50 +0100

    [backend] add findByArtistArtistId method to EventRepository

--- Backend/src/main/java/nebel/backend/repo/EventRepo.java
+++ Backend/src/main/java/nebel/backend/repo/EventRepo.java
@@ -3,5 +3,8 @@ package nebel.backend.repo;
 import nebel.backend.pojo.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EventRepo extends JpaRepository<Event, Long> {
+    List<Event> findByArtistArtistId(Long artistId);
 }
