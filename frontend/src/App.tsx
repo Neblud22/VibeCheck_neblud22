b2271e92276cc7fe00c172a90259ca72f03807c4
neblud22 <neblud22@htl-kaindorf.at>
2026-03-11 07:51:52 +0100

    [frontend] commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {Navigate, Route, Routes} from "react-router-dom";
+import ArtistOverview from "./components/ArtistOverview.tsx";
+import EventOverview from "./components/EventOverview.tsx";
+import EventDetail from "./components/EventDetail.tsx";
+import NavBar from "./components/NavBar.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <NavBar/>
+        <Routes>
+            <Route path="/" element={<ArtistOverview />} />
+            <Route path="/artists" element={<ArtistOverview />} />
+            <Route path="/events" element={<EventOverview />} />
+            <Route path="/events/artist/:artistId" element={<EventOverview />} />
+            <Route path="/events/:eventId" element={<EventDetail />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
     </>
   )
 }
